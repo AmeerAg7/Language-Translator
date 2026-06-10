91e8e5c2fec0babc0afae4c1ed8682265987fa52
AmeerAg7 <agameer1@gmail.com>
2026-06-11 01:42:34 +0300

    test the ci pipeline

--- app.py
+++ app.py
@@ -77,4 +77,4 @@ def translate():
 
 #### Our main function which runs the Flask App
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True
